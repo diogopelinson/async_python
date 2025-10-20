b77c134ecf6c98fa693dc0e074fd9875bef18208
Diogo <diogopelinsonduartemoraes@gmail.com>
2025-10-20 01:04:54 -0300

    feat: Adicionando mais exemplos de assincronismo, agora utilzando fastapi com uvicorn no servidor

---
+++ exemplo3/run_1.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import time
+
+#Sincronismo
+
+app= Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    time.sleep(30)
+    return { "hello": "world"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, threaded=False)
---
+++ exemplo3/run_2.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn #Servidor assincrono
+import asyncio
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    await asyncio.sleep(20)
+    return { "hello": "world" }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
